b422ae0f55a54ec056fcf65e6604d84936f6a2b1
beatriz-lima <beatriz.f.l@hotmail.com>
2020-05-25 12:28:39 +0100

    Updated query

--- sql/query_bookings.sql
+++ sql/query_bookings.sql
@@ -1,11 +1,16 @@
-CREATE OR REPLACE FUNCTION query_bookings ()
-RETURNS TABLE(season integer, count bigint) AS $$
+DROP FUNCTION query_bookings();
+CREATE OR REPLACE FUNCTION query_bookings()
+RETURNS TABLE(date_id integer, season varchar(10), count bigint, coastal varchar(20)) AS $$
 BEGIN
-	RETURN QUERY SELECT date_id, COUNT(*)
-	FROM booking 
-	GROUP BY date_id
-	ORDER BY count DESC;
+	RETURN QUERY SELECT b.date_id, d.season, count(*), l.coastal_area
+	FROM date d, booking b, location l
+	WHERE b.date_id = d.date_id
+	AND b.location_id = l.location_id
+	AND l.coastal_area = 'Not Coastal Area'
+	GROUP BY b.date_id, d.season, l.coastal_area
+	ORDER BY count DESC
+	LIMIT 50;
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT * from query_bookings ();
+SELECT * FROM query_bookings();
